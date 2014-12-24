d9381ae3bbe85e5c6b79233eaf0f0619221de43e
Christian Mamuad <cmamuad@paypal.com>
2014-12-23 21:58:58 -0800

    Basic requirements

--- TipCalculator/TipCalculator/SettingsViewController.swift
+++ TipCalculator/TipCalculator/SettingsViewController.swift
@@ -9,14 +9,29 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-  
-    @IBOutlet weak var DefaultPercentageField: UITextField!
+    
+    @IBOutlet weak var LowTextField: UITextField!
+    @IBOutlet weak var MidTextField: UITextField!
+    @IBOutlet weak var HighTextField: UITextField!
+    @IBOutlet weak var SaveButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("viewDidLoad initiated for SettingsViewController")
+        var percentages : [Int] = SettingsViewController.getDefaultPercentages()
+        LowTextField.text = String(percentages[0])
+        MidTextField.text = String(percentages[1])
+        HighTextField.text = String(percentages[2])
+        
     }
     
+    @IBAction func lightThemeClicked(sender: AnyObject) {
+        saveDefaultTheme("light")
+        SettingsViewController.setTheme(self.view, theme: "light")
+    }
+    @IBAction func darkThemeClicked(sender: AnyObject) {
+         saveDefaultTheme("dark")
+        SettingsViewController.setTheme(self.view, theme: "dark")
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -24,7 +39,119 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func onSaveClicked(sender: AnyObject) {
+        var low :Int = LowTextField.text.toInt()!
+        var mid : Int = MidTextField.text.toInt()!
+        var high :Int = HighTextField.text.toInt()!
+        
+        saveDefaultPercentages(low, mid:mid, high:high)
+        var animate : Bool = true;
+        navigationController?.popViewControllerAnimated(animate)
     }
     
+    func saveDefaultTheme(color:(String)){
+        var defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(color, forKey: "theme")
+        defaults.synchronize()
+    }
+    
+    class func getDefaultTheme() -> String {
+        var defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        var theme: String! = defaults.stringForKey("theme")
+        if (theme == nil){
+            theme = "light"
+        }
+        return theme;
+    }
+    
+    func saveDefaultPercentages(low:(Int), mid:(Int), high:(Int)){
+        var defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(low, forKey: "low")
+        defaults.setInteger(mid, forKey: "mid")
+        defaults.setInteger(high, forKey: "high")
+        defaults.synchronize()
+    }
+    
+    class func getDefaultPercentages() -> [Int]{
+        var defaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        var low  : Int = defaults.integerForKey("low")
+        var mid  : Int = defaults.integerForKey("mid")
+        var high : Int = defaults.integerForKey("high")
+        var percentages : [Int] = [15,18,20]
+        
+        if(low > 0 && mid > 0 && high > 0){
+            //no problems with saved info
+            percentages = [low, mid, high]
+        }
+        return percentages
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        var theme: String = SettingsViewController.getDefaultTheme()
+        
+        SettingsViewController.setTheme(self.view, theme: theme)
+    }
+    
+    class func setTheme(rootView:UIView, theme: (String)){
+        SettingsViewController.setViewStyle(rootView, theme: theme)
+        SettingsViewController.setThemeRecursively(rootView, theme: theme)
+    }
+    
+    class func setThemeRecursively(rootView:UIView, theme: (String)){
+        if let textField = rootView as? UITextField {
+            setTextFieldStyle(textField, theme: theme)
+        }else if let lb = rootView as? UILabel {
+            setLabelStyle(lb, theme: theme)
+        }
+        
+        for v in rootView.subviews as [UIView] {
+            SettingsViewController.setThemeRecursively(v, theme: theme)
+        }
+
+    }
+    
+    class func setViewStyle(view:UIView, theme:String){
+        if(theme == "dark"){
+            view.backgroundColor = UIColor.blackColor()
+        }else{
+            view.backgroundColor = UIColor.whiteColor()
+        }
+    }
+    
+    class func setLabelStyle(label:UILabel, theme:String){
+        if(theme == "dark"){
+           label.textColor = UIColor.whiteColor()
+        }else{
+            label.textColor = UIColor.blackColor()
+        }
+    }
+    
+    class func setTextFieldStyle(control:UITextField, theme:String){
+        if(theme == "dark"){
+            control.textColor = UIColor.whiteColor()
+            control.backgroundColor = UIColor(netHex: 0x006600)
+            
+        }else{
+            control.textColor = UIColor.blackColor()
+            control.backgroundColor = UIColor(netHex: 0x33FF33)
+        }
+    }
+ 
+
+    
+}
+
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(netHex:Int) {
+        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
+    }
 }
 
--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -14,21 +14,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var BillAmount: UITextField!
     @IBOutlet weak var TotalAmountLabel: UILabel!
     @IBOutlet weak var TipPercentageSegment: UISegmentedControl!
+    var tipPercentages : [Int]!;
+    @IBOutlet weak var peopleSlider: UISlider!
+    @IBOutlet weak var peopleCountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         TipAmountLabel.text = "0.00"
         TotalAmountLabel.text = "0.00"
-       
         self.navigationItem.setRightBarButtonItem(UIBarButtonItem(title: "Settings", style: UIBarButtonItemStyle.Plain, target: self, action: "onSettingsClicked"), animated: true)
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func onSettingsClicked(){
-        println("On Settings Clicked triggered.")
-        self.navigationController?.pushViewController(SettingsViewController(), animated: true)
+    //slider control
+    @IBAction func onSliderChanged(sender: AnyObject) {
+        view.endEditing(true)
+        peopleCountLabel.text = String(Int(peopleSlider.value))
+        calculateValues()
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -38,17 +42,71 @@ class ViewController: UIViewController {
             view.endEditing(true)
     }
     
+    //tip selector control
+    @IBAction func onTipSelected(sender: AnyObject) {
+        view.endEditing(true)
+        calculateValues()
+    }
+    
+    //on keyboard press
     @IBAction func onEditingChanged(sender: AnyObject) {
-        var tipPercentages = [0.15, 0.18, 0.20]
-        var tipPercentage = tipPercentages[TipPercentageSegment.selectedSegmentIndex]
+        calculateValues()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        println("View Will Appear called")
+        resetTipPercentages()
+        calculateValues()
+        var theme: String = SettingsViewController.getDefaultTheme()
+        SettingsViewController.setViewStyle(self.view, theme: theme)
+        SettingsViewController.setTheme(self.view, theme: theme)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        println("View Did Appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        println("View Will Disappear")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        println("View did Disappear")
+    }
+
+    
+    
+    
+    func resetTipPercentages(){
+        TipPercentageSegment.removeAllSegments()
+        tipPercentages = SettingsViewController.getDefaultPercentages()
         
-        println("Editing the BillAmount")
+        for index in 0...tipPercentages.count-1{
+            var title = String(tipPercentages[index]) + "%"
+            TipPercentageSegment.insertSegmentWithTitle(title, atIndex: index, animated: true)
+        }
+        TipPercentageSegment.selectedSegmentIndex = 0
+        
+        
+    }
+    
+    func onSettingsClicked(){
+        println("On Settings Clicked triggered.")
+        var viewController : UIViewController = self.navigationController?.storyboard?.instantiateViewControllerWithIdentifier("SettingsViewController") as UIViewController
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    func calculateValues(){
+        var tipPercentage : Double = Double(tipPercentages[TipPercentageSegment.selectedSegmentIndex]) / Double(100)
+
+        var numberOfPeople = Int(peopleSlider.value)
         var billAmount = (BillAmount.text as NSString).doubleValue
-        var tip = billAmount * tipPercentage
-        var total = billAmount + tip
+        var tip = (billAmount * tipPercentage) / Double(numberOfPeople)
+        var total = (billAmount + tip) / Double(numberOfPeople)
         TipAmountLabel.text = String(format: "$%.2f", tip)
         TotalAmountLabel.text = String(format: "$%.2f", total)
     }
 
+    
 }
 
